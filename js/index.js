ffd2b6de574146b21c7a7f3188c0abd45eaafa5c
dongismeee <betapchoi1708@gmail.com>
2021-09-21 11:57:51 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let abc = true;
+let inputpassword = prompt("Hãy nhập đúng password")
+while(true){
+if(inputpassword === "minhtrang0807"){
+    alert("Chin chào bạn, ban rất là tuyệt vời");
+    break;
+}else{
+    inputpassword = prompt("Ơ kia! bạn nhập sai rồi :(")
+
+}
+}
